e888d4d4a305203f773ccdf7d862d8fd02e1f785
RayChenLDL <40652062+RayChenLDL@users.noreply.github.com>
2021-09-11 08:45:46 +1000

    Update SamplePageUtilities.java

--- src/test/java/com/roames/test/utilities/SamplePageUtilities.java
+++ src/test/java/com/roames/test/utilities/SamplePageUtilities.java
@@ -6,6 +6,7 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.concurrent.TimeUnit;
 import org.testng.ITestContext;
+import com.relevantcodes.extentreports.LogStatus;
 
 import org.apache.commons.configuration.ConfigurationException;
 import org.apache.commons.lang3.StringUtils;
